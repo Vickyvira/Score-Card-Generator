f132e0499bf29be5cb38451f79746569b0460413
Vicky Kumar Das <43489006+Vickyvira@users.noreply.github.com>
2021-08-29 15:37:20 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,62 @@
+// alert ('Welcome to the JavaScript');
+// console.error("this is an error")
+// console.warn("this is a warning")
+
+// var, let, const
+
+// let age = 30;
+// age = 22
+
+
+// console.log(age);
+
+// String, Numbers, Boolean, null, undefined
+
+// const name = 'Vicky';
+// const age = 30;
+// const rating = 4.5;
+// const isCool = true;
+// const x = null;
+// const y = undefined;
+// let z;
+
+// console.log(typeof rating);
+
+
+// const name = 'Vicky';
+// const age = 22;
+
+// //Concatenation
+// console.log('My name is ' + name + ' and I am ' + age);
+// //Template String
+// const hello = 'My name is ${name} and I am ${age}';
+
+// console.log(hello);
+
+// const s = 'Computer, Science, Code, Js';
+
+// console.log(s.toUpperCase());
+// console.log(s.substring(0, 5).toUpperCase()); HELLO
+
+// console.log(s.split(', '));
+
+// Arrays - that holds multiple values
+
+// const fruits = ['apples', 'oranges', 'pears'];
+
+// fruits[3] ='grapes';
+// fruits[4] = 'mango';
+
+// fruits.push('Eggs')
+// fruits.unshift('strawberries');
+// fruits.pop();
+// console.log(Array.isArray('hello'));
+// console.log(fruits.indexOf('oranges '));
+
+// console.log(fruits);
+
+// carName = "Volvo, red";
+// alert(carName);
+// var carName;
+
+// int
